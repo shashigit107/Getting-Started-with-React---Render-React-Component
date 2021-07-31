b1ad15a1b1f533f4e8c321e62f2757334b6d31af
shashigit107 <83776652+shashigit107@users.noreply.github.com>
2021-08-01 01:03:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div id="root">
-       <p>"I am learning React. My life is getting better."</p>
+       <p>I am learning React. My life is getting better.</p>
     </div>
   );
 }
